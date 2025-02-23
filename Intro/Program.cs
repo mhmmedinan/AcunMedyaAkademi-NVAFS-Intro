d41f3f68e095cef9905be4fae004705d47a3bed2
mhmmedinan <minan.65@hotmail.com>
2025-02-23 11:52:20 +0300

    Diziler,Döngüler

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -96,15 +96,140 @@ string correctPassword = "123456";
 
 
 
-Console.Write("Enter your role (admin/user) : ");
-string role = Console.ReadLine().ToLower();
+//Console.Write("Enter your role (admin/user) : ");
+//string role = Console.ReadLine().ToLower();
 
-bool isAdmin = (role == "admin");
-if (isAdmin)
-    Console.WriteLine("Access granted. Welcome!");
+//bool isAdmin = (role == "admin");
+//if (isAdmin)
+//    Console.WriteLine("Access granted. Welcome!");
+//else
+//    Console.WriteLine("Access denied!");
+
+
+
+
+
+//Loops
+
+//for,while,foreach,do-while
+
+for(int i = 0; i<20; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+for(int i = 0; i < 25; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine($" Tek Sayılar : {i}");
+    }
+}
+
+
+//diziler sabit boyutludur.
+//string[] products = new string[3];
+//products[0] = "Laptop";
+//products[1] = "Mouse";
+//products[2] = "Keyboard";
+
+
+//for(int i = 0; i < products.Length; i++)
+//{
+//    Console.WriteLine($"Ürünler : {products[i]}");
+//}
+
+//string[] products = { "Laptop", "Mouse", "Keyboard" };
+//products[3] = "Monitor";                    // dizinin boyutunu aştığı için hata verir.
+//foreach(string product in products)
+//{
+//     Console.WriteLine($"Ürünler : {product}");
+//}
+
+
+//Console.Write("Dizi boyutunu giriniz : ");
+//int arraySize = Convert.ToInt32(Console.ReadLine());
+
+//int[] numbers = new int[arraySize];
+//for(int i = 0; i < arraySize; i++)
+//{
+//    Console.Write("Sayı giriniz : ");
+//    numbers[i] = Convert.ToInt32(Console.ReadLine());
+//}
+
+//Console.WriteLine("Girilen Sayılar : ");
+//foreach(int number in numbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+
+//list
+//<T> generic yapıdır. T herhangi bir veri tipi olabilir.
+//List<string> cities = new List<string>();
+//cities.Add("Ankara");
+//cities.Add("İstanbul");
+//cities.Add("İzmir");
+
+//foreach(string city in cities)
+//{
+//    Console.WriteLine(city);
+//}
+
+//Console.WriteLine("-------------------------");
+
+//cities.Add("Adana");
+//foreach (string city in cities)
+//{
+//    Console.WriteLine(city);
+//}
+
+//Console.WriteLine("-------------------------");
+//cities.Remove("Ankara");
+//foreach (string city in cities)
+//{
+//    Console.WriteLine(city);
+//}
+
+
+List<string> cities = new List<string> {"Ankara", "İstanbul", "İzmir","Ankara"};
+foreach (string city in cities)
+{
+    Console.WriteLine(city);
+}
+cities.RemoveAll(city=>city == "Ankara");
+
+Console.WriteLine("-------------------------");
+foreach (string city in cities)
+{
+    Console.WriteLine(city);
+}
+
+
+List<string> baskets = new List<string> { "Apple", "Banana", "Orange", "Apple" };
+
+foreach (string basket in baskets)
+{
+    Console.WriteLine(basket);
+}
+
+Console.Write("Silmek istediğiniz ürünü giriniz : ");
+string product = Console.ReadLine();
+
+if (baskets.Contains(product))
+{
+    baskets.Remove(product);
+    Console.WriteLine($" {product} isimli ürün sepetten silindi.");
+}
 else
-    Console.WriteLine("Access denied!");
+{
+    Console.WriteLine($" {product} isimli ürün sepetinizde bulunamadı.");
+}
 
+Console.WriteLine("Sepetinizdeki ürünler : ");
+foreach (string basket in baskets)
+{
+    Console.WriteLine(basket);
+}
 
-
-    
